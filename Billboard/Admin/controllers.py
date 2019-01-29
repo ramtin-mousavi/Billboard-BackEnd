bb7faa079c2d9f9ca0c42756c1698173e0a0e09a
Ramtin Mousavi <ramtin.mousavi@ymail.com>
2019-01-29 11:45:45 +0330

    Add Admin_Required decorator to admin functions

--- Billboard/Admin/controllers.py
+++ Billboard/Admin/controllers.py
@@ -38,154 +38,139 @@ class Admin:
     @staticmethod
     @cross_origin(supports_credentials=True)
     @login_required
+    @Admin_Required (['apps'])
     def get_pending_apps():
 
-        if session ['role'] == 'admin':
-
-            apps = Android_Model.query_ ('pending')
-            out = {'apps':Android_Model.serialize_many(apps), 'status':'OK'}
-            return jsonify (out)
 
 
-        out = {'apps':'', 'status':'asscess denied'}
-        return jsonify(out)
+        apps = Android_Model.query_ ('pending')
+        out = {'apps':Android_Model.serialize_many(apps), 'status':'OK'}
+        return jsonify (out)
+
+
+
 
 
     @staticmethod
     @cross_origin(supports_credentials=True)
     @login_required
+    @Admin_Required (['app', 'submit'])
     def approve_or_reject_apps (submit , app_id):
 
-        if session ['role'] == 'admin':
 
-            app = Android_Model.query.get (int (app_id))
+        app = Android_Model.query.get (int (app_id))
 
-            if app:
+        if app:
 
-                if submit == 'approve':
+            if submit == 'approve':
 
-                    app.approve()
-                    out = {'app':app.serialize_one() ,'submit':'approve','status':'OK'}
-                    return jsonify (out)
-
-
-                elif submit == 'reject':
-
-                    app.reject()
-                    out = {'app':app.serialize_one() ,'submit':'reject','status':'OK'}
-                    return jsonify (out)
-
-                out = {'app':'', 'submit':'', 'status':'submit command is wrong'}
+                app.approve()
+                out = {'app':app.serialize_one() ,'submit':'approve','status':'OK'}
                 return jsonify (out)
 
-            out = {'app':'', 'submit':'', 'status':'app id is wrong'}
+
+            elif submit == 'reject':
+
+                app.reject()
+                out = {'app':app.serialize_one() ,'submit':'reject','status':'OK'}
+                return jsonify (out)
+
+            out = {'app':'', 'submit':'', 'status':'submit command is wrong'}
             return jsonify (out)
 
-        out = {'app':'', 'submit':'' ,'status':'asscess denied'}
-        return jsonify(out)
+        out = {'app':'', 'submit':'', 'status':'app id is wrong'}
+        return jsonify (out)
+
 
 
     @staticmethod
     @cross_origin(supports_credentials=True)
     @login_required
+    @Admin_Required (['surveys'])
     def get_pending_surveys ():
 
-        if session ['role'] == 'admin':
-
-            surveys = Survey_Model.query_('pending')
-            out = {'surveys':Survey_Model.serialize_many(surveys), 'status':'OK'}
-            return jsonify (out)
+        surveys = Survey_Model.query_('pending')
+        out = {'surveys':Survey_Model.serialize_many(surveys), 'status':'OK'}
+        return jsonify (out)
 
 
-        out = {'surveys':'', 'status':'asscess denied'}
-        return jsonify(out)
 
 
     @staticmethod
     @cross_origin(supports_credentials=True)
     @login_required
+    @Admin_Required (['survey', 'submit'])
     def approve_or_reject_surveys (submit , survey_id):
 
-        if session ['role'] == 'admin':
+        survey = Survey_Model.query.get (int (survey_id))
 
-            survey = Survey_Model.query.get (int (survey_id))
+        if survey:
 
-            if survey:
+            if submit == 'approve':
 
-                if submit == 'approve':
-
-                    survey.approve()
-                    out = {'survey':survey.serialize_one() ,'submit':'approve','status':'OK'}
-                    return jsonify (out)
-
-
-                elif submit == 'reject':
-
-                    survey.reject()
-                    out = {'survey':survey.serialize_one() ,'submit':'reject','status':'OK'}
-                    return jsonify (out)
-
-                out = {'survey':'', 'submit':'', 'status':'submit command is wrong'}
+                survey.approve()
+                out = {'survey':survey.serialize_one() ,'submit':'approve','status':'OK'}
                 return jsonify (out)
 
-            out = {'survey':'', 'submit':'', 'status':'app id is wrong'}
+
+            elif submit == 'reject':
+
+                survey.reject()
+                out = {'survey':survey.serialize_one() ,'submit':'reject','status':'OK'}
+                return jsonify (out)
+
+            out = {'survey':'', 'submit':'', 'status':'submit command is wrong'}
             return jsonify (out)
 
-        out = {'survey':'', 'submit':'' ,'status':'asscess denied'}
-        return jsonify(out)
+        out = {'survey':'', 'submit':'', 'status':'app id is wrong'}
+        return jsonify (out)
+
 
 
     @staticmethod
     @cross_origin(supports_credentials=True)
     @login_required
+    @Admin_Required (['user_count'])
     def get_users_count ():
 
-        if session ['role'] == 'admin':
-            out = {'user_count':User_Model.query.count(), 'status':'OK'}
-            return jsonify (out)
-
-        out = {'user_count':'', 'status':'access denied'}
+        out = {'user_count':User_Model.query.count(), 'status':'OK'}
         return jsonify (out)
+
+
 
 
     @staticmethod
     @cross_origin(supports_credentials=True)
     @login_required
+    @Admin_Required (['ads_count'])
     def active_ads_count ():
 
-        if session['role'] == 'admin':
-            surveys = Survey_Model.query_ ('approved').count()
-            apps = Android_Model.query_ ('approved').count()
+        surveys = Survey_Model.query_ ('approved').count()
+        apps = Android_Model.query_ ('approved').count()
 
-            out = {'ads_count':surveys + apps, 'status':'OK'}
-            return jsonify (out)
-
-        out = {'ads_count':'', 'status':'access denied'}
+        out = {'ads_count':surveys + apps, 'status':'OK'}
         return jsonify (out)
+
 
 
     @staticmethod
     @cross_origin(supports_credentials=True)
     @login_required
+    @Admin_Required (['today_ads_count'])
     def today_ads_count ():
 
-        if session['role'] == 'admin':
+        today = datetime.now()
+        today_start = datetime (today.year, today.month, today.day, 00, 00, 00)
+        today_stop = datetime (today.year, today.month, today.day, 23, 59, 59)
 
-            today = datetime.now()
-            today_start = datetime (today.year, today.month, today.day, 00, 00, 00)
-            today_stop = datetime (today.year, today.month, today.day, 23, 59, 59)
+        count = (Android_Model.query.filter (Android_Model.advertise_date > today_start,
+                Android_Model.advertise_date < today_stop).count()
+                )
+        count += (Survey_Model.query.filter (Survey_Model.advertise_date > today_start,
+                Survey_Model.advertise_date < today_stop).count()
+                )
 
-            count = (Android_Model.query.filter (Android_Model.advertise_date > today_start,
-                    Android_Model.advertise_date < today_stop).count()
-                    )
-            count += (Survey_Model.query.filter (Survey_Model.advertise_date > today_start,
-                    Survey_Model.advertise_date < today_stop).count()
-                    )
-
-            out = {'today_ads_count':count, 'status': 'OK'}
-            return jsonify (out)
-
-        out = {'today_ads_count':'', 'status':'access denied'}
+        out = {'today_ads_count':count, 'status': 'OK'}
         return jsonify (out)
 
 
